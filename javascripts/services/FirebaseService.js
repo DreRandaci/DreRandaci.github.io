380cab3706b38c957fd087b15e12901062e8e816
Dre mf Randaci <drerandaci@gmail.com>
2018-01-19 19:26:36 -0600

    project styles

--- javascripts/services/FirebaseService.js
+++ javascripts/services/FirebaseService.js
@@ -1,22 +1,24 @@
 'use strict';
 
-app.service("FirebaseService", function( $http, $q, FIREBASE_CONFIG ){
+app.service("FirebaseService", function ($http, $q, FIREBASE_CONFIG) {
 
     const getAllProjects = () => {
         let projects = [];
-        return $q(( resolve, reject ) => {
-            $http.get(`${FIREBASE_CONFIG.databaseURL}/projects.json`).then(( results ) => {
+        return $q((resolve, reject) => {
+            $http.get(`${FIREBASE_CONFIG.databaseURL}/projects.json`).then((results) => {
                 let fbProjects = results.data;
-                Object.keys(fbProjects).forEach(( key ) => {
-                    fbProjects[key].id = key;
-                    projects.push(fbProjects[key]);                    
-                });
+                if (fbProjects) {
+                    Object.keys(fbProjects).forEach((key) => {
+                        fbProjects[key].id = key;
+                        projects.push(fbProjects[key]);
+                    });
+                }
                 resolve(projects);
             }).catch((err) => {
                 console.log('error in getAllProjects:', err);
             });
         });
-    };    
+    };
 
     return { getAllProjects };
 });
